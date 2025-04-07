5eca8f72967d6ebcf375ec514d0d90c1d3403a85
Shotech <40011872+Shotech-E@users.noreply.github.com>
2025-04-07 13:14:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 
 // Enable CORS for Vercel deployment
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "https://cmf-frontend-one.vercel.app",
   credentials: true
 }));
 
