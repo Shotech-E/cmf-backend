a9869f460697c4c673213cf4ab808234eeeb9fc4
Shotech <showars1990@gmail.com>
2025-04-07 09:52:27 +0100

    changes made

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "cmf-frontend.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
